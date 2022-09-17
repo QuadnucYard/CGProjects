3d57ee865c0fcb789c14020e2f21179d840c60d3
QuadnucYard <2380433991@qq.com>
2022-09-17 12:18:33 +0800

    一个乱七八糟的环

--- CGProjects/main.cpp
+++ CGProjects/main.cpp
@@ -4,6 +4,9 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <cmath>
+#include <numbers>
+#include <random>
 #include "Utils.h"
 using namespace std;
 
@@ -18,22 +21,41 @@ GLuint vbo[numVBOs];
 GLuint ebo[numVBOs];
 GLuint vColorLoc, vPositionLoc;
 
+constexpr int numDiv = 36;
+constexpr float r1 = 0.4f, r2 = 0.7f;
+
 //导入着色器，初始化窗口
 void init(GLFWwindow* window)
 {
 	//编译、链接着色器程序
 	renderingProgram = Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");
 
-	float vertices[] = {
-		0.6f, 0.6f, 0.2f, 0.1f, 0.9f,
-		-0.5f, 0.5f, 0.3f, 0.8f, 0.6f,
-		-0.6f, -0.6f, 0.5f, 0.2f, 0.8f,
-		0.5f, -0.5f, 0.9f, 0.5f, 0.2f,
-	};
-	GLuint  indices[] = {
-		0, 1, 2, // 左上
-		2, 3, 0, // 右下
-	};
+	float vertices[numDiv * 2 * 5] {};
+	GLuint indices[numDiv * 2 * 3] {};
+
+	default_random_engine rng;
+	uniform_real_distribution<float> distrib(0.0f, 1.0f);
+	normal_distribution<float> distrib2(0.0f, 0.05f);
+
+	using namespace std::numbers;
+	for (int i = 0; i < numDiv; i++) {
+		vertices[i * 10 + 0] = (r2 + distrib2(rng)) * cos(pi * 2 * i / numDiv);
+		vertices[i * 10 + 1] = (r2 + distrib2(rng)) * sin(pi * 2 * i / numDiv);
+		vertices[i * 10 + 2] = distrib(rng);
+		vertices[i * 10 + 3] = distrib(rng);
+		vertices[i * 10 + 4] = distrib(rng);
+		vertices[i * 10 + 5] = (r1 + distrib2(rng)) * cos(pi * 2 * i / numDiv);
+		vertices[i * 10 + 6] = (r1 + distrib2(rng)) * sin(pi * 2 * i / numDiv);
+		vertices[i * 10 + 7] = distrib(rng);
+		vertices[i * 10 + 8] = distrib(rng);
+		vertices[i * 10 + 9] = distrib(rng);
+		indices[i * 6 + 0] = (i * 2 + 0) % (numDiv * 2);
+		indices[i * 6 + 1] = (i * 2 + 1) % (numDiv * 2);
+		indices[i * 6 + 2] = (i * 2 + 2) % (numDiv * 2);
+		indices[i * 6 + 3] = (i * 2 + 3) % (numDiv * 2);
+		indices[i * 6 + 4] = (i * 2 + 1) % (numDiv * 2);
+		indices[i * 6 + 5] = (i * 2 + 2) % (numDiv * 2);
+	}
 
 	// Associate out shader variables with our data buffer    
 	vPositionLoc = glGetAttribLocation(renderingProgram, "vPosition");
@@ -67,7 +89,7 @@ void display(GLFWwindow* window, double currentTime)
 
 	//绘制模型
 	glBindVertexArray(vao[0]);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glDrawElements(GL_TRIANGLES, 6 * numDiv, GL_UNSIGNED_INT, 0);
 }
 
 int main(void)
@@ -82,7 +104,7 @@ int main(void)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //次版本号
 
 	//创建窗口
-	GLFWwindow* window = glfwCreateWindow(800, 800, "simple", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Not simple", NULL, NULL);
 	glfwMakeContextCurrent(window);
 	//设置缓存刷新时间
 	glfwSwapInterval(1);
