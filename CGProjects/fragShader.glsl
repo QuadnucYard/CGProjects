e9b48e058c74d774f53222abf24587e778e3e664
QuadnucYard <2380433991@qq.com>
2022-09-22 18:49:51 +0800

    入门项目更名

--- CGProjects/fragShader.glsl
+++ CGProjects/fragShader.glsl
@@ -1,9 +1,11 @@
 #version 430
 
 in vec3 outColor;
+layout(location = 0) out vec4 diffuseColor;
 
 void main()
 {
-	gl_FragColor = vec4(outColor, 1.0);
+	diffuseColor = vec4(outColor, 1.0);
+//	gl_FragColor = vec4(outColor, 1.0);
 }
 
