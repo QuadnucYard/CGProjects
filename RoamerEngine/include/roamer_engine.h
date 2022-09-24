69f4ad42fc5b2e13032dfc2f29f7359bd356b79d
QuadnucYard <2380433991@qq.com>
2022-09-24 10:18:14 +0800

    Add Application

---
+++ RoamerEngine/include/roamer_engine.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "roamer_engine/Application.h"
+#include "roamer_engine/display/MeshRenderer.h"
---
+++ RoamerEngine/include/roamer_engine/Application.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <cstdlib>
+#include <string_view>
+
+namespace qy::cg {
+
+	class Application {
+
+	public:
+		static int VERSION_MAJOR;
+		static int VERSION_MINOR;
+
+
+	private:
+		GLFWwindow* window;
+
+	public:
+		Application() {
+			//glfw初始化
+			if (!glfwInit()) {
+				std::exit(EXIT_FAILURE);
+			}
+			//窗口版本
+			glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, VERSION_MAJOR); //主版本号
+			glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, VERSION_MINOR); //次版本号
+			window = nullptr;
+		}
+
+		~Application() {
+			if (window) glfwDestroyWindow(window);
+			glfwTerminate();
+			exit(EXIT_SUCCESS);
+		}
+
+		Application* createWindow(int width, int height, std::string_view title) {
+			window = glfwCreateWindow(800, 800, title.data(), nullptr, nullptr);
+			glfwMakeContextCurrent(window);
+			//设置缓存刷新时间
+			glfwSwapInterval(1);
+			//glew初始化
+			if (glewInit() != GLEW_OK) {
+				exit(EXIT_FAILURE);
+			}
+			return this;
+		}
+
+		Application* run() {
+			init();
+			//事件循环，接收输入事件
+			while (!glfwWindowShouldClose(window)) {
+				display(); //绘制函数，主体
+				glfwSwapBuffers(window); //交换颜色缓存
+				glfwPollEvents(); // 检查有没有触发什么事件（比如键盘输入、鼠标移动等）
+			}
+			return this;
+		}
+
+	protected:
+
+		virtual void init() = 0;
+		virtual void display() = 0;
+	};
+}
+
+int qy::cg::Application::VERSION_MAJOR = 4;
+int qy::cg::Application::VERSION_MINOR = 3;
