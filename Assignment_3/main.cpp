fc6e2b18c3ea3e485a963d06a75361cb6e88c704
Stone <1592277253@qq.com>
2022-10-23 18:46:37 +0800

    Assignment_3, control pov with mouse and createCylinder

--- Assignment_3/main.cpp
+++ Assignment_3/main.cpp
@@ -6,9 +6,11 @@
 using namespace qy::cg;
 std::shared_ptr<DisplayObject> createCylinder(int numDiv = 50)
 {
-	auto obj = DisplayObject().create();
+	auto obj = DisplayObject::create();
 	auto mr = obj->addComponent<MeshRenderer>();
-	auto&& mesh = mr->getMesh();
+	mr->setMaterial(Materials::geom_unlit);
+	auto mf = obj->addComponent<MeshFilter>();
+	auto mesh = mf->mesh();
 
 	std::vector<glm::vec3> vertices(numDiv * 2 + 2);
 	std::vector<GLuint> triangles(numDiv * 4 * 3);
@@ -38,11 +40,28 @@ std::shared_ptr<DisplayObject> createCylinder(int numDiv = 50)
 	}
 
 	using namespace qy::cg;
-	mesh.setVertices(vertices);
-	mesh.setTriangles(triangles);
-	mesh.setColors(vcolors);
+	mesh->setVertices(vertices);
+	mesh->setTriangles(triangles);
+	mesh->setColors(vcolors);
 	return obj;
 	return obj;
+}
+
+glm::quat RotU2V(glm::vec3 start, glm::vec3 dest) {
+	start = normalize(start);
+	dest = normalize(dest);
+	float cosTheta = dot(start, dest);
+	glm::vec3 rotationAxis;
+
+	rotationAxis = cross(start, dest);
+	float s = sqrt((1 + cosTheta) * 2);
+	float invs = 1 / s;
+	return glm::quat(
+		s * 0.5f,
+		rotationAxis.x * invs,
+		rotationAxis.y * invs,
+		rotationAxis.z * invs
+	);
 }
 
 class MyApplication : public qy::cg::Application {
@@ -54,6 +73,12 @@ private:
 	glm::vec3 camFront{ 0, 0, -1 };
 	glm::vec3 camUp{ 0, 1, 0 };
 
+	glm::vec3 posLast = { 0.0f, 0.0f, 0.0f };
+	bool mouseLeftPressed = false;
+	float sensitivity = 0.1f;
+	float yaw = -90.0f;
+	float pitch = 0.0f;
+
 protected:
 	void init() override {
 		using namespace qy::cg;
@@ -64,13 +89,13 @@ protected:
 
 		scene = Scene::create();
 		cam = scene->createCamera();
-		cam->obj()->transform()->position({ 1, 1, 1 });
+		cam->obj()->transform()->position({ 0, 0, 1 });
 
 		auto obj = Primitives::createCube();
 		obj->transform()->position({ 0.5f, 0, 0 });
 		obj->transform()->scale({ 0.3f, 0.3f, 0.3f });
 		obj->transform()->rotation(glm::vec3(0.3f, 0.2f, 0.6f));
-		obj->getComponent<MeshRenderer>()->getMesh().setColors(
+		obj->getComponent<MeshFilter>()->mesh()->setColors(
 			{ {0, 0, 0, 0}, {.5f, 0, 0, 1}, {0, .5f, 0, 1}, {0, 0, .5f, 1}, {.5f, .5f, 0, 1}, {.5f, 0, .5f, 1}, {0, .5f, .5f, 1}, {.5f, .5f, .5f, 1} });
 		scene->root()->addChild(obj->transform());
 		//
@@ -83,21 +108,61 @@ protected:
 		obj = createCylinder();
 		obj->transform()->scale({ 0.1f, 0.1f, 0.1f });
 		scene->root()->addChild(obj->transform());
+
 	}
 
 	void update() override {
 		using namespace qy::cg;
 		scene->dispatch_update();
 
-		float cameraSpeed = Time::deltaTime() * 5;
+		float cameraSpeed = Time::deltaTime()*3;
 		auto camPos = cam->obj()->transform()->position();
+
 		if (Input::getKeyDown(KeyCode::W)) camPos += cameraSpeed * camFront;
 		if (Input::getKeyDown(KeyCode::S)) camPos -= cameraSpeed * camFront;
 		if (Input::getKeyDown(KeyCode::A)) camPos -= glm::normalize(glm::cross(camFront, camUp)) * cameraSpeed;
 		if (Input::getKeyDown(KeyCode::D)) camPos += glm::normalize(glm::cross(camFront, camUp)) * cameraSpeed;
 		if (Input::getKeyDown(KeyCode::SPACE)) camPos += camUp * cameraSpeed;
 		if (Input::getKeyDown(KeyCode::LEFT_SHIFT)) camPos -= camUp * cameraSpeed;
+		
+
+		if (Input::getMouseButtonDown(GLFW_MOUSE_BUTTON_LEFT) && !mouseLeftPressed) {
+			posLast = Input::mousePosition();
+			mouseLeftPressed = true;
+		}
+		if (Input::getMouseButtonUp(GLFW_MOUSE_BUTTON_LEFT)) {
+			mouseLeftPressed = false;
+		}
+		if (Input::getMouseButtonDown(GLFW_MOUSE_BUTTON_LEFT)) {
+			glm::vec3 posNow = Input::mousePosition();
+			glm::vec3 posDelta = posNow - posLast;
+			posLast = posNow;
+			float xOffset = posDelta.x * sensitivity;
+			float yOffset = - posDelta.y * sensitivity;
+			yaw += xOffset;
+			pitch += yOffset;
+
+			if (pitch > 89.0f)
+				pitch = 89.0f;
+			if (pitch < -89.0f)
+				pitch = -89.0f;
+
+			glm::vec3 front;
+			front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+			front.y = sin(glm::radians(pitch));
+			front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+			camFront = glm::normalize(front);
+
+			std::cout << "posLast: " << posLast.x << " | " << posLast.y << " | " << posLast.z << std::endl;
+			//std::cout << "rot: " << rot0.w << " | " << rot0.x << " | " << rot0.y << " | " << rot0.z << std::endl;
+			std::cout << "camfront: " << camFront.x << " | " << camFront.y << " | " << camFront.z << std::endl << std::endl;
+			std::cout << "camPos: " << camPos.x << " | " << camPos.y << " | " << camPos.z << std::endl;
+
+		}
+
 		cam->transform()->position(camPos);
+
+		cam->transform()->rotation(RotU2V({0.0, 0.0, -1.0}, camFront));
 		cam->setFieldOfView(std::clamp(cam->getFieldOfView() + Input::mouseScrollDelta().y, 1.0f, 80.0f));
 	}
 
