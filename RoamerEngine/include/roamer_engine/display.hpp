f2734cc4f248b514310fea93fb96f630f018895e
QuadnucYard <2380433991@qq.com>
2022-10-24 19:03:57 +0800

    Fix input and header include

--- RoamerEngine/include/roamer_engine/display.hpp
+++ RoamerEngine/include/roamer_engine/display.hpp
@@ -2,7 +2,6 @@
 #include "display/DisplayObject.hpp"
 #include "display/MeshRenderer.hpp"
 #include "display/MeshFilter.hpp"
-#include "display/LineRenderer.hpp"
 #include "display/Shaders.hpp"
 #include "display/Scene.hpp"
 #include "display/Primitives.hpp"
