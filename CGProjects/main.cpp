940c95d7d315342d3ab616d10c0fc361f7b54841
QuadnucYard <2380433991@qq.com>
2022-10-24 20:03:32 +0800

    Update sample

--- CGProjects/main.cpp
+++ CGProjects/main.cpp
@@ -15,25 +15,26 @@ private:
 protected:
 	void init() override {
 		using namespace qy::cg;
-		glEnable(GL_BLEND);
+		/*glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 		glEnable(GL_DEPTH_TEST);
-		glDepthFunc(GL_LEQUAL);
-
+		glDepthFunc(GL_LEQUAL);*/
 		scene = Scene::create();
 		cam = scene->createCamera();
-		cam->obj()->transform()->position({1, 1, 1});
+		cam->setBackgroundColor({0.4f, 0.3f, 0.1f, 1.0f});
+		cam->obj()->transform()->position({0, 0, 5});
 		auto obj = Primitives::createCube();
-		obj->transform()->position({0.5f, 0, 0});
+		obj->transform()->position({1.0f, 0, 0});
 		obj->transform()->scale({0.3f, 0.3f, 0.3f});
 		obj->transform()->rotation(glm::vec3(0.3f, 0.2f, 0.6f));
-		obj->getComponent<MeshRenderer>()->getMesh().setColors(
-			{{0, 0, 0, 0}, {.5f, 0, 0, 1}, {0, .5f, 0, 1}, {0, 0, .5f, 1}, {.5f, .5f, 0, 1}, {.5f, 0, .5f, 1}, {0, .5f, .5f, 1}, {.5f, .5f, .5f, 1}});
+		obj->getComponent<MeshFilter>()->mesh()->setColors(
+			{{0, 0, 0, 1}, {.5f, 0, 0, 1}, {0, .5f, 0, 1}, {0, 0, .5f, 1}, {.5f, .5f, 0, 1}, {.5f, 0, .5f, 1}, {0, .5f, .5f, 1}, {.5f, .5f, .5f, 1}});
 		scene->root()->addChild(obj->transform());
-		obj = Primitives::createCube();
+		obj = Primitives::createSphere();
 		obj->transform()->position({0.1f, 0.3f, -0.2f});
 		obj->transform()->scale({0.3f, 0.1f, 0.4f});
 		obj->transform()->rotation(glm::vec3(0.7f, -0.3f, 0.1f));
+		obj->getComponent<MeshRenderer>()->getMaterial()->setColor("_Color", Color::rgba(222, 143, 228, 127));
 		scene->root()->addChild(obj->transform());
 	}
 
@@ -41,14 +42,14 @@ protected:
 		using namespace qy::cg;
 		scene->dispatch_update();
 		
-		float cameraSpeed = Time::deltaTime() * 5;
+		float cameraSpeed = (float)Time::deltaTime() * 5;
 		auto camPos = cam->obj()->transform()->position();
-		if (Input::getKeyDown(KeyCode::W)) camPos += cameraSpeed * camFront;
-		if (Input::getKeyDown(KeyCode::S)) camPos -= cameraSpeed * camFront;
-		if (Input::getKeyDown(KeyCode::A)) camPos -= glm::normalize(glm::cross(camFront, camUp)) * cameraSpeed;
-		if (Input::getKeyDown(KeyCode::D)) camPos += glm::normalize(glm::cross(camFront, camUp)) * cameraSpeed;
-		if (Input::getKeyDown(KeyCode::SPACE)) camPos += camUp * cameraSpeed;
-		if (Input::getKeyDown(KeyCode::LEFT_SHIFT)) camPos -= camUp * cameraSpeed;
+		if (Input::getKey(KeyCode::W)) camPos += cameraSpeed * camFront;
+		if (Input::getKey(KeyCode::S)) camPos -= cameraSpeed * camFront;
+		if (Input::getKey(KeyCode::A)) camPos -= glm::normalize(glm::cross(camFront, camUp)) * cameraSpeed;
+		if (Input::getKey(KeyCode::D)) camPos += glm::normalize(glm::cross(camFront, camUp)) * cameraSpeed;
+		if (Input::getKey(KeyCode::SPACE)) camPos += camUp * cameraSpeed;
+		if (Input::getKey(KeyCode::LEFT_SHIFT)) camPos -= camUp * cameraSpeed;
 		cam->transform()->position(camPos);
 		cam->setFieldOfView(std::clamp(cam->getFieldOfView() + Input::mouseScrollDelta().y, 1.0f, 80.0f));
 	}
@@ -60,7 +61,7 @@ protected:
 
 int main() {
 	MyApplication app;
-	app.createWindow(800, 800, "MyApplication");
+	app.createWindow(1280, 720, "MyApplication");
 	app.run();
 	return 0;
 }
