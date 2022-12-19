f6f301822c1d174e33a8fa5c73870dce039459b0
Stone <1592277253@qq.com>
2022-12-19 20:49:33 +0800

    fixed the roll increase in moveControl

--- RoamerEngine/src/roamer_engine/display/MoveController.cpp
+++ RoamerEngine/src/roamer_engine/display/MoveController.cpp
@@ -146,15 +146,24 @@ namespace qy::cg {
 
 		const float pi = std::numbers::pi_v<float>;
 		auto yaw = -xOffset / 1800 * pi * pImpl->sensitivity;
-		glm::quat rot_yaw = {glm::cos(yaw), glm::sin(yaw)*pImpl->up};
+		glm::quat rotYaw = {glm::cos(yaw), glm::sin(yaw)*pImpl->up};
+		auto rotNew1 = glm::normalize(rotYaw * rot);
+		front = rotNew1 * pImpl->front_init;
+
 		auto v = glm::normalize(glm::cross(front, pImpl->up));
 		auto pitch = yOffset / 1800 * pi * pImpl->sensitivity;
-		glm::quat rot_pitch = glm::quat {cos(pitch), sin(pitch) * v};
-		auto rotNew = glm::normalize(rot_pitch * rot_yaw * rot);
-		if ((rotNew * pImpl->front_init).y > 0.99 || (rotNew * pImpl->front_init).y <- 0.99)
-			rotNew = rot_yaw * rot;
+		glm::quat rotPitch = glm::quat {cos(pitch), sin(pitch) * v};
+		auto rotNew2 = glm::normalize(rotPitch * rotYaw * rot);
+		if ((rotNew2 * pImpl->front_init).y > 0.99 || (rotNew2 * pImpl->front_init).y <- 0.99)
+			rotNew2 = rotYaw * rot;
+
+		/*auto roll = glm::eulerAngles(rotNew).z;
+		if (!((roll > -0.01 && roll<0.01) || roll > 3.13 || roll <-3.13)) {
+			auto rotRoll = glm::quat {cos(-roll), sin(-roll) * glm::vec3 {0, 0, 1}};
+			rotNew = rotRoll * rotNew;
+		}*/
 		transform()->position(objPos);
-		transform()->rotation(rotNew);
+		transform()->rotation(rotNew2);
 
 		auto cam = getComponent<Camera>();
 		if (cam) {
