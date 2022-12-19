0df672a422e7a719d9b2ea1ffd7da8b60bf06e77
Stone <1592277253@qq.com>
2022-12-19 17:45:01 +0800

    add transform change in transformEditor

--- RoamerEditor/src/roamer_editor/TransformEditor.cpp
+++ RoamerEditor/src/roamer_editor/TransformEditor.cpp
@@ -6,8 +6,8 @@ namespace qy::cg::editor {
 	void TransformEditor::onInspectorGUI() {
 		auto&& transform = lockTarget<Transform>();
 
-		DragFloat3("Position", transform->position(), 0.1f);
-		DragFloat3("Scale", transform->scale(), 0.01f);
-		DragFloat3("Rotation", glm::eulerAngles(transform->rotation()), 0.01f);
+		transform->position(DragFloat3("Position", transform->position(), 0.1f));
+		transform->scale(DragFloat3("Scale", transform->scale(), 0.01f));
+		transform->rotation(DragFloat3("Rotation", glm::eulerAngles(transform->rotation()), 0.01f));
 	}
 }
